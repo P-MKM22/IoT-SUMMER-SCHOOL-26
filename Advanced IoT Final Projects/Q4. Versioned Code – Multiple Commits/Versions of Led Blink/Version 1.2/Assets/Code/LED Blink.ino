5318cdb27b6563f93df1672b8bd36a39014bf4db
P MEET KUMAR <pkmgenai24@gmail.com>
2026-07-07 23:55:04 +0530

    Fix syntax errors and enhance Serial print formatting
    Corrected syntax errors and improved print statements.

--- Advanced IoT Final Projects/Q4. Versioned Code – Multiple Commits/Versions of Led Blink/Version 1.2/Assets/Code/LED Blink.ino
+++ Advanced IoT Final Projects/Q4. Versioned Code – Multiple Commits/Versions of Led Blink/Version 1.2/Assets/Code/LED Blink.ino
@@ -1,24 +1,4 @@
-const int const int Pin = 9;
-const int potPin = A0;
-
-int blinkCount = 0;
-int sensorValue = 0;
-int blinkDelay = 0;
-
-void setup() {
-  pinMode(Pin, OUTPUT);
-
-  Serial.begin(9600);
-  Serial.println("LED Blink Speed Controller");
-}
-
-void loop() {
-
-  sensorValue = analogRead(potPin);
-
-  blinkDelay = map(sensorValue, 0, 1023, 100, 1000);
-
-  digitalWrite(const int Pin = 9;
+const int Pin = 9;
 const int potPin = A0;
 
 int blinkCount = 0;
@@ -46,63 +26,13 @@ void loop() {
 
   blinkCount++;
 
-  Serial.print("Blink Count : ");
+  Serial.print("Blink Count: ");
   Serial.print(blinkCount);
 
-  Serial.print(" | Potentiometer : ");
+  Serial.print(" | Potentiometer: ");
   Serial.print(sensorValue);
 
-  Serial.print(" | Delay : ");
-  Serial.print(blinkDelay);
-  Serial.println(" ms");
-}, HIGH);
-  delay(blinkDelay);
-
-  digitalWrite(Pin, LOW);
-  delay(blinkDelay);
-
-  blinkCount++;
-
-  Serial.print("Blink Count : ");
-  Serial.print(blinkCount);
-
-  Serial.print(" | Potentiometer : ");
-  Serial.print(sensorValue);
-
-  Serial.print(" | Delay : ");
-  Serial.print(blinkDelay);
-  Serial.println(" ms");
-} = 9;
-const int potPin = A0;
-
-int blinkCount = 0;
-int sensorValue = 0;
-int blinkDelay = 0;
-
-void setup() {
-  pinMode(Pin, OUTPUT);
-
-  Serial.begin(9600);
-  Serial.println("LED Blink Speed Controller");
-}
-
-void loop() {
-
-  sensorValue = analogRead(potPin);
-  blinkDelay = map(sensorValue, 0, 1023, 100, 1000);
-
-  digitalWrite(Pin, HIGH);
-  delay(blinkDelay);
-
-  digitalWrite(Pin, LOW);
-  delay(blinkDelay);
-
-  blinkCount++;
-  Serial.print("Blink Count : ");
-  Serial.print(blinkCount);
-  Serial.print(" | Potentiometer : ");
-  Serial.print(sensorValue);
-  Serial.print(" | Delay : ");
+  Serial.print(" | Delay: ");
   Serial.print(blinkDelay);
   Serial.println(" ms");
 }
